0b78a52977b03c62cb70ac66510818930073e691
Aayush Joshi <aayushjoshi129@gmail.com>
2024-01-18 22:00:57 +0530

    First class learning data analytics using python (1hr)

---
+++ Programs/studentDetails.py
@@ -0,0 +1,5 @@
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+roll_no=input("Enter your roll_no: ")
+
+print("Hi ", name, " your age is ", age, " and your roll no is ", roll_no, " isn't this right?")
---
+++ userInput.py
@@ -0,0 +1,14 @@
+a=input("Enter your name:")
+print(a)
+age=int(input("Enter your age:"))
+print(age)
+
+# used to evaluate any expression
+exp1 = eval(input("Enter Expression:"))
+print(exp1)
+
+# typeCasting
+x='123'
+print(type(x))
+x=int(x)
+print("after conversion: ", type(x))
