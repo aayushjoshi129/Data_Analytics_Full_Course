26322949836080d9776f28bb07db5d9ce86305e2
Aayush Joshi <aayushjoshi129@gmail.com>
2024-02-03 18:44:54 +0530

    Ninth Day Learning Data Analytics Using Python (1.5hr)

---
+++ Functions_and_Modules/Day 9/demo.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    return x+y
+
+def mult(x,y):
+    return x*y
+
+def subtract(x,y):
+    return x-y
+
+def divide(x,y):
+    if y==0:
+        return 'Division By 0 is Not Possible'
+    else:
+        return x/y
+
+def exp(x,y):
+    return x**y
+
+
+Employee = {'Name':'Karan', 'Age':24, 'Occupation':'Data Anayst'}
---
+++ Numpy/Day 9/Array.py
@@ -0,0 +1,95 @@
+# An Array is defined as a collection of items that are stored at the contiguous emmory locations
+
+# It is a container which can hold a fix number of items, and these items should be of same type.
+
+# A combination of arrays saves a lot of time. They can reduce the overall size of the code.
+
+# It uses much less memory. It is used for creation of n-dim arrays.
+
+# finding elements in numpy array is easy.
+
+
+# CREATION, INDEXING AND SLICING OF NUMPY ARRAYS
+
+import numpy as np
+
+'''
+arr = np.array([10,20,30,40])
+
+print(arr)
+print(type(arr))
+print(arr[0:3])
+print(arr[::-1])
+
+print(arr.shape)
+'''
+
+'''
+arr2 = np.array([[10,20,30,40], [50,60,70,80]])
+
+print(arr2)
+print(type(arr2))
+print(arr2[0:2, 0:2])
+print(arr2[0:3, 0:3])
+print(arr2[0, 0:2])
+print(arr2[1, 0:2])
+
+print(arr2.shape)
+print(arr2.size)
+print(arr2.ndim)
+print(arr2.dtype)
+
+'''
+
+# INSPECTING AN ARRAY
+
+'''
+arr = np.array([10,20,30,40])
+arr2 = np.array([[10,20,30,40], [50,60,70,80]])
+
+# length
+print(len(arr))
+print(len(arr2))
+
+# shape
+print(arr.shape)
+print(arr2.shape)
+
+# dimension
+print(arr.ndim)
+print(arr2.ndim)
+
+# dtype
+print(arr.dtype)
+print(arr2.dtype)
+
+# size
+print(arr.size)
+print(arr2.size)
+
+# casting
+print(arr.astype(float))
+print(arr2.astype(float))
+'''
+
+
+# MATHEMATICAL OPERATIONS AND FUNCTIONS ON ARRAYS
+
+'''
+arr1 = np.array([10,20,30,40])
+arr2 = np.array([[10,20,30,40], [50,60,70,80]])
+arr3 = np.array([3])
+
+print(arr1 + arr2)
+print(np.add(arr1, arr2))
+print(np.subtract(arr2, arr1))
+print(np.multiply(arr1, arr2))
+print(np.divide(arr1, arr2))
+print(np.power(arr1, arr3))
+print(np.sqrt(arr1))
+print(np.sqrt(arr2))
+'''
+
+# COMBINING AND SPLITTING IN ARRAYS
+
+
