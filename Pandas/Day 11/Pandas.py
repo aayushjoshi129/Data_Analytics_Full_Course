5f7bcfd836610b03916ba3eec979010cf39ff092
Aayush Joshi <aayushjoshi129@gmail.com>
2024-02-07 22:52:29 +0530

    Eleventh Day Learning Data Analytics Using Python (45 minutes)

---
+++ Pandas/Day 11/Pandas.py
@@ -0,0 +1,35 @@
+# Pandas is a python package providing fast, flexible amd expressive datastructures designed to make working with 'relational' or 'labelled' data both easy and intuitive.
+
+# Here are just a few of the things that pandas does well:
+# It has functions for analyzing, cleaning, exploring, and manipulating data.
+
+# The name "Pandas" has a reference to both "Panel Data", and "Python Data Analysis" and was created by Wes McKinney in 2008
+
+
+# Pandas Applications
+
+# Easy handling of missing data
+# Size Mutability: columns can be inserted and deleted from DataFrame and higher dimensional data
+# Automatic and explicit data alignment: Objects can be explicitly aligned to a set of labels, or the user can simply ignore the labels and let Series, DataFrames, etc.
+
+# Automatically align the data for you in computations
+# powerful, flexible group by functionality
+# Intelligent label-based slicing, fancy indexing, and sub-setting of larger data sets.
+# Intuitive merging and joining datasets
+# Flexible reshaping and pivoting of datasets.
+
+
+# Data Structures
+# The best way to think about the pandas data structure is as flexible containers for lower dimensional data.
+# For Ex- Data Frame is a container for series, and series is a containers for scalars.
+# we would like to be able to insert and remove objects form these containers in a dictionary-like fashion.
+
+# Series in Pandas
+# Pandas series is a 1-D Labelled array capable of holding data types of any type. The axis labels are collectively called index. Pandas series is nothing but a column in an excel sheet.
+
+# The Object supports both integer and label-based indexing and provides a host of methods for performing operations involving the index.
+
+
+
+# DATA FRAMES
+# Pandas DF is a 2D size-mutable, potentially heterogeneous tabular data structure with labelled axes (rows and columns). A DF is a 2D data structure, i.e., data is aligned in a tabular fashion in rows and columns. Pandas DF consist of 3 principal components i.e., the data, the rows and the columns.
